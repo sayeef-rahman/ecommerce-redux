c750510a76cb3898e96c31e58595b0a1cb079358
sayeef-rahman <sayeef.cse@gmail.com>
2022-07-23 14:57:17 +0600

    footer modified

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faIdCard } from '@fortawesome/free-solid-svg-icons';
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <footer className='py-5 footer-background-color' id='footer'>
+            <div className='mx-auto'>
+                <div className='d-flex'>
+                    <div>
+                        <h6>ABOUT US</h6>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Phasellus imperdiet, nulla et dictum interdum, nisi lorem
+                            egestas vitae scel
+                        </p>
+                    </div>
+                    <div>hi</div>
+                    <div></div>
+                    <div></div>
+                </div>
+                {/* Bottom text & icom element */}
+                <div className='d-flex border-top border-secondary pt-4 justify-content-between align-items'>
+                    <div>
+                        <h6>Copyright © 2022 Sindabad. All Rights Reserved</h6>
+                    </div>
+                    <div>
+                        <FontAwesomeIcon
+                            className='fs-3 mx-1'
+                            icon={faIdCard}
+                        ></FontAwesomeIcon>
+                        <FontAwesomeIcon
+                            className='fs-3 mx-1'
+                            icon={faIdCard}
+                        ></FontAwesomeIcon>
+                        <FontAwesomeIcon
+                            className='fs-3 mx-1'
+                            icon={faIdCard}
+                        ></FontAwesomeIcon>
+                        <FontAwesomeIcon
+                            className='fs-3 mx-1'
+                            icon={faIdCard}
+                        ></FontAwesomeIcon>
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 };
 
