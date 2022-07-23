a41058001bd2af074a55b3e0de4111049449c075
sayeef-rahman <sayeef.cse@gmail.com>
2022-07-23 16:48:20 +0600

    products viewed in homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Home.css'
+import Products from './Products';
 import Slider from './Slider/Slider';
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
+            <Products></Products>
         </div>
     );
 };
---
+++ src/components/Home/Products.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import Product from './Product';
+import './Products.css';
+
+const Products = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+            .then((res) => res.json())
+            .then((data) => setProducts(data));
+    }, []);
+    return (
+        <div className='product-container'>
+            {/* Title & All Product button section */}
+            <div className='d-flex justify-content-between w-75 mx-auto'>
+                <div>
+                    <h2>Our Products</h2>
+                </div>
+                <div>
+                    <button className='btn-viewAll'>View All</button>
+                </div>
+            </div>
+
+            {/* all products card */}
+            <div className='container'>
+                <div className='row'>
+                    {products.slice(0, 10).map((product) => (
+                        <Product key={product.id} product={product}></Product>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
