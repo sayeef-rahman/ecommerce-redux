fb8817af85ccf6afccfd6ea350ebe2cb9f385148
sayeef-rahman <sayeef.cse@gmail.com>
2022-07-23 13:46:11 +0600

    react firebase hooks insatalled

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import { Routes, Route, Link } from "react-router-dom";
+import Footer from "./components/Shared/Footer/Footer";
+import Navbar from "./components/Shared/Navbar/Navbar";
 function App() {
   return (
     <div className="">
-      
+      <Navbar></Navbar>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Navbar = () => {
+const Footer = () => {
     return (
         <div>
-            <h1>Navbar</h1>
+            <h1>Footer</h1>
         </div>
     );
 };
 
-export default Navbar;
+export default Footer;
